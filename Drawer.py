afaa81146926bd75820a237f43f5da8596510c00
CoffeeBrewer <cas15151@uvg.edu.gt>
2019-05-14 03:14:25 -0600

    Comenzando de nuevo

--- Drawer.py
+++ Drawer.py
@@ -2,6 +2,9 @@
 from PIL import ImageTk, Image, ImageDraw
 import PIL
 from tkinter import *
+import numpy as np
+
+from red import RR
 
 width = 400
 height = 400
@@ -54,4 +57,34 @@ root.mainloop()
 img = PIL.Image.open("image.bmp")
 img = img.resize((28, 28), PIL.Image.ANTIALIAS)
 img.save('image.bmp')
+
+content = PIL.Image.open("image.bmp").convert("I")
+array_content = np.array(content);
+#Array_Content es un array numerico que tiene los valores de los colores al reves
+
+for col in array_content:
+    for row in col:
+        row = (255 - row) / 255.0
+
+input = array_content.reshape(784, 1)
+red = RR([5, 4, 3, 2])
+
+
+#Se usara la formula de
+#       Nh = (Ns)/(alpha * (Ni + No)), donde
+
+#       Nh      =   cantidad de neuronas en las hidden layers
+#       alpha   =   Un numero arbitrario, usualmente entre 2 a 10
+#       Ni      =   Numero de neuronas input
+#       No      =   Numero de neuronas output
+#       Ns      =   Numero de neuronas del test
+
+#Siendo los valores de estos
+
+#       Nh      = [364, 243, 189, 146. 121. 104, 91, 80, 73]
+#       alpha   = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+#       Ni      = 784
+#       No      = 10
+#       Ns      = 577672
+
 
--- red.py
+++ red.py
@@ -1,94 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import random
 
-
-class RR(object):
-    def __init__(self, tamanios):
-        self.capas = len(tamanios)
-        self.tamanios = tamanios
+class RR:
+    def __init__(self, tamanio):
+        self.weigths = []
         self.biases = []
-        self.weight = []
-        self.assign_value()
-        self.assign_weights()
-
-    def assign_value(self):
-        i = 1
-        for i in range(len(self.tamanios)):
-            random = np.random.randn(i, 1)
-            self.biases.append(random)
-
-    def assign_weights(self):
-        for x, y in zip(self.tamanios[:-1], self.tamanios[1:]):
-            random = np.random.rand(x, y)
-            self.weight.append(random)
-        return
-
-    def feed_forward(self, input):
-        for b, w in zip(self.biases, self.weight):
-            a = sigmoid(np.dot(w, a) + b)
-        return a
-
-    def cost_gradient(self, neuronas, expectancy):
-        return neuronas - expectancy
-
-    def delta_precision(self, resultado, experanza):
-        test_results = [(np.argmax(self.feed_forward(x)), y)
-                       for (x, y) in resultado]
-        return sum(int(x == y) for (x, y) in test_results)
 
 
-def sigmoid(x):
-    return 1/(1 + np.exp(-x))
+        if len(tamanio) > 1:
+            for i in range(len(tamanio) - 1):
+                w1 = np.random.rand(tamanio[i + 1], tamanio[i])
+                b1 = np.random.randn(tamanio[i + 1], 1)
 
+                self.weigths.append(w1)
+                self.biases.append(b1)
+        else:
+            print("La red neuronal debe de tener al menos una capa, aparte de la inicial")
+        pass
 
-def sigmoid_derivate(x):
-    return sigmoid(x) * (1-sigmoid(x))
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
-
-
-
-def back_propagation():
-    return
-
-
-
-
-
-#Se usara la formula de
-#       Nh = (Ns)/(alpha * (Ni + No)), donde
-
-#       Nh      =   cantidad de neuronas en las hidden layers
-#       alpha   =   Un numero arbitrario, usualmente entre 2 a 10
-#       Ni      =   Numero de neuronas input
-#       No      =   Numero de neuronas output
-#       Ns      =   Numero de neuronas del test
-
-#Siendo los valores de estos
-
-#       Nh      = [364, 243, 189, 146. 121. 104, 91, 80, 73]
-#       alpha   = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-#       Ni      = 784
-#       No      = 10
-#       Ns      = 577672
-
-data = np.load('../data/all_data.npy')
-cant_data = len(data)
-batches = []
-
-#PArtir en 3 el dataset y revolverlo
-int1 = int(np.round(0.70*cant_data))
-int2 = int(np.round(0.85*cant_data))
-int3 = cant_data -1
-
-np.random.shuffle(data)
-
-train = data[:int1]
-test = data[int1:int2]
-CV = data[int2:]
-
-RR([784, 189, 91, 10])
-
-
-
-
+    def feed_forward(self, x):
+        for i in range(len(self.weigths)):
+            a = self.sigmoid((self.weigths[i] @ x) + self.biases[i])
+            x = a
+        return x
